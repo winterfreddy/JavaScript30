2e2bb2d2336006a7a2c3ce5789ad310937e62880
Winfred Huang <huang.winfred21@gmail.com>
2020-10-16 22:12:17 -0700

    Standardize code

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -29,8 +29,9 @@ function paintToCanvas() {
         ctx.drawImage(video, 0, 0, width, height);
         let pixels = ctx.getImageData(0, 0, width, height);
         // pixels = redEffect(pixels);
-        pixels = rgbSplit(pixels);
-        ctx.globalAlpha = 0.1;
+        // pixels = rgbSplit(pixels);
+        // pixels = greenScreen(pixels);
+        // ctx.globalAlpha = 0.1;
         ctx.putImageData(pixels, 0, 0);
     }, 20);
 }
