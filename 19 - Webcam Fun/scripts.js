67a9357aae1599707c5fb24f231a04d07255fbeb
Winfred Huang <huang.winfred21@gmail.com>
2020-10-16 22:10:42 -0700

    green screen code

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -69,6 +69,33 @@ function rgbSplit(pixels) {
     return pixels;
 }
 
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        // take it out!
+        pixels.data[i + 3] = 0;
+        }
+    }
+
+    return pixels;
+}
+
 getVideo();
 
 video.addEventListener('canplay', paintToCanvas);
