f7520bc55d2d0e0d98b958f29f885e42073502c5
Winfred Huang <huang.winfred21@gmail.com>
2020-10-26 23:57:03 -0700

    Allow user to enter minutes and start timer immediately

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -50,3 +50,11 @@ function startTimer() {
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
+
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+})
