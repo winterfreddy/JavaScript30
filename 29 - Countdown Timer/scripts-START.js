12c54fffb36bcdb52c746bc73ad7ec46342685cb
Winfred Huang <huang.winfred21@gmail.com>
2020-10-26 23:40:12 -0700

    Display when timer ends

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
+    displayEndTime(then);
 
     countdown = setInterval( () => {
         const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -25,6 +27,15 @@ function displayTimeLeft(seconds) {
     const remainderSeconds = seconds % 60;
 
     const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    
+    document.title = display; // puts the timer in the tab
     timerDisplay.textContent = display;
+}
 
+function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const minutes = end.getMinutes();
+
+    endTime.textContent = `Be back at ${hour > 12 ? hour - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
